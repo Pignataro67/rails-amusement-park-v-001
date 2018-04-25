4ac0b62b85a6147fa6e3ee2d622247c803f8b5e0
Pat  Pignataro <pat6284@msn.com>
2018-04-25 15:46:50 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(
       :name,
-      :height,
+      #:height,
       :height,
       :nausea,
       :tickets,
